39ee2314cce38996e68617947912555747c608bb
Yann Menoud <111496797+menoudyann@users.noreply.github.com>
2022-11-04 08:50:32 +0100

    Update Twitter.php

--- src/main/php/Twitter.php
+++ src/main/php/Twitter.php
@@ -3,36 +3,58 @@
 namespace SocialNetwork;
 
 use RuntimeException;
+use function PHPUnit\Framework\throwException;
 
 require 'IObservable.php';
 
 class Twitter implements IObservable
 {
-    public function __construct(array $observers = null){}
+    protected $observers = [];
+    protected $twits = [];
+
+    public function __construct(array $observers = []){
+        $this->observers = $observers;
+    }
 
     public function subscribe(array $observers):void
     {
-        throw new RuntimeException();
+        foreach ($observers as $observer) {
+            if (in_array($observer, $this->observers, true)){
+                throw new SubscriberAlreadyExistsException();
+            }
+            $this->observers[] = $observer;
+        }
     }
 
     public function unsubscribe(IObserver $observer):void
     {
-        throw new RuntimeException();
+        if(count($this->observers)>0) {
+            if (in_array($observer, $this->observers, true)) {
+                $id = array_search($observer, $this->observers);
+                unset($this->observers[$id]);
+            }else{
+                throw new SubscriberNotFoundException();
+            }
+        }else{
+            throw new EmptyListOfSubscribersException();
+        }
     }
 
     public function notifyObservers():void
     {
-        throw new RuntimeException();
+        if(count($this->observers) == 0){
+            throw new EmptyListOfSubscribersException();
+        }
     }
 
     public function getObservers():array
     {
-        throw new RuntimeException();
+        return $this->observers;
     }
 
     public function getTwits():array
     {
-        throw new RuntimeException();
+        return $this->twits;
     }
 }
 
